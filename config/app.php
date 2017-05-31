4d3d1980374fddaafb1defcc7f17071aef499da6
Marques <marquesde2013@gmail.com>
2017-05-31 16:05:23 -0300

    Implementação de Illuminate\Html com novas facades

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
-
+        Illuminate\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -228,7 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Html' => Illuminate\Html\HtmlFacade::class,
+        'Form' => Illuminate\Html\FormFacade::class,
     ],
 
 ];
