8b8474d401713660bb5453abf09d8d00f4d2b780
Marques <marquesde2013@gmail.com>
2017-06-01 09:57:12 -0300

    token

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,9 +11,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create([
+        factory(\App\Entities\User::class)->create([
             'email' => 'jonasjunior@ifto.edu.br',
             'name'  => 'jonas'
         ]);
+        factory(App\Entities\User::class, 100)->create();
+        factory(App\Entities\TypeUser::class, 5)->create();
+        factory(App\Entities\UserTypeUser::class, 100)->create();
+        factory(App\Entities\Event::class, 100)->create();
+        factory(App\Entities\TypeActivity::class, 100)->create();
+        factory(App\Entities\Activity::class, 10)->create();
+        factory(App\Entities\TypeActivityUser::class, 5)->create();
+        factory(App\Entities\ActivityUser::class, 5)->create();
+        factory(App\Entities\Article::class,100)->create();
+        factory(App\Entities\Participation::class,100)->create();
+        factory(App\Entities\Instution::class,10)->create();
+        factory(App\Entities\Course::class,10)->create();
+
+        // Erro nessa dois
+        factory(App\Entities\CourseEvent::class,5)->create();
+        factory(App\Entities\UserEvent::class,5)->create();
     }
 }
