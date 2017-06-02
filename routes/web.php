888fa7ba9112ba2304c448f52c8378ed2ca5a472
Administrador <pedrocesar1010@gmail.com>
2017-06-02 06:15:39 -0300

    view edi_user.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('teste', function () {
+    return view('edi_user');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
