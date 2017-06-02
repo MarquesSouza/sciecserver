a7c70c683b7b5859a823e6cb897c50bb9571c0c0
T-Gamer <marquesde2013@gmail.com>
2017-06-02 04:29:46 -0300

    correção instutions

--- resources/views/atividade/cad_atividade.blade.php
+++ resources/views/atividade/cad_atividade.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('app');
 
-@section('content')
+@section('content');
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -11,87 +11,9 @@
                         <legend>
                             <center>Cadastrar Atividade</center>
                         </legend>
-
-                        <!-- Nome da Atividade-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="atividade">Nome</label>
-                            <div class="col-md-5">
-                                <input id="atividade" name="nome" type="text" placeholder="Nome da Atividade"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Descrição da Atividade-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="atividade">Descrição</label>
-                            <div class="col-md-5">
-                                <input id="descricao" name="descricao" type="text" placeholder="Descrição da Atividade"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Hora da Atividade-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="atividade">Hora</label>
-                            <div class="col-md-5">
-                                <input id="hora" name="hora" type="time" placeholder="Horário da Atividade"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Local da Atividade-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="local">Local</label>
-                            <div class="col-md-5">
-                                <input id="local" name="local" type="text" placeholder="Local da Atividade"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Dtq de Inscritos-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="qtd_inscritos">Quantidade de Inscritos</label>
-                            <div class="col-md-5">
-                                <input id="hora" name="qtd_inscritos" type="text" placeholder="Quantidade de Inscritos"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Local da Atividade-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="cod_inscircao">Código de Inscrição</label>
-                            <div class="col-md-5">
-                                <input id="local" name="local" type="text" placeholder="Local da Atividade"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Data de Início-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="data_inicio">Data de Início</label>
-                            <div class="col-md-5">
-                                <input id="data_inicio" name="data_inicio" type="text" placeholder="Data de Início"
-                                       class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-                        <!-- Data de Conclusão-->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="data_conclusao">Data de Conclusão</label>
-                            <div class="col-md-5">
-                                <input id="data_conclusao" name="data_conclusao" type="text"
-                                       placeholder="Data de Conclusão" class="form-control input-md" required="">
-
-                            </div>
-                        </div>
-
-                        <!-- Button -->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="cadastrar"></label>
-                            <div class="col-md-4">
-                                <button id="cadastrar" name="cadastrar" class="btn btn-success">Cadastrar</button>
-                            </div>
-                        </div>
+                        @include('form._form1');
+                        @include('form._form2');
+                        @include('atividade._form');
                     </fieldset>
                 </form>
             </div>
---
+++ resources/views/form/_form1.blade.php
@@ -0,0 +1,16 @@
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" >Nome</label>
+                            <div class="col-md-5">
+                                <input id="nome" name="nome" type="text" placeholder="Nome"
+                                       class="form-control input-md" required="">
+
+                            </div>
+                        </div>
+                         <div class="form-group">
+                            <label class="col-md-4 control-label">Descrição</label>
+                            <div class="col-md-5">
+                                <input id="descricao" name="descricao" type="text" placeholder="Descrição da Atividade"
+                                       class="form-control input-md" required="">
+
+                            </div>
+                        </div>
