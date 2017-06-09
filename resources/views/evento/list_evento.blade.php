b122b6660494e30e45d7075956b7af9099a8b706
Marques <marquesde2013@gmail.com>
2017-06-09 15:12:46 -0300

    ativação da autenticação

--- resources/views/evento/list_evento.blade.php
+++ resources/views/evento/list_evento.blade.php
@@ -18,16 +18,20 @@
                     <th>Data de Início </th>
                     <th>Data de Conclusão</th>
                     <th>Logo do Evento</th>
+
                      <tr>
                         @forelse ($events as $ev)
+                             <form action="{{url('evento/inscricao_evento/'.$ev->id)}}" method="post">
+                                 {{csrf_field()}}
 
-                            <td>{{ $ev->nome }}</td>
+                                 <td>{{ $ev->nome }}</td>
                             <td>{{ $ev->descricao }}</td>
                             <td>{{ $ev->local }}</td>
                             <td>{{ $ev->data_inicio }}</td>
                             <td>{{ $ev->data_conclusao }}</td>
                             <td>{{ $ev->logoEvento }}</td>
-
+                                 <td><button type="submit" >Confirmar Inscrição</button></td>
+                             </form>
                     </tr>
                     @empty
                         <p>No events</p>
