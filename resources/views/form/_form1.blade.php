b4eac434ba8e4293eff87e46b9e908433ca69c73
RômuloSousa <romulo.rmos@hotmail.com>
2017-06-11 20:13:19 -0300

    Alteração Request Na função Store da Tabela TypeUsers

--- resources/views/form/_form1.blade.php
+++ resources/views/form/_form1.blade.php
@@ -1,7 +1,7 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" >Nome</label>
                             <div class="col-md-5">
-                                <input id="nome" name="nome" type="text" placeholder="Nome"
+                                <input id="nome" name="nome" type="text" placeholder=""
                                        class="form-control input-md" required="">
 
                             </div>
@@ -9,7 +9,7 @@
                          <div class="form-group">
                             <label class="col-md-4 control-label">Descrição</label>
                             <div class="col-md-5">
-                                <input id="descricao" name="descricao" type="text" placeholder="Descrição da Atividade"
+                                <input id="descricao" name="descricao" type="text" placeholder=""
                                        class="form-control input-md" required="">
 
                             </div>
