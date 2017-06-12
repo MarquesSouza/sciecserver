21fef313a8a8a811992584d7a104d99228fbce32
krkbjj <krkbjj@gmail.com>
2017-06-11 21:25:31 -0300

    Mudança e Otimização do Template Correção forms evento/cad Correção de Botões e Rotas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,74 +1,119 @@
-@extends('app')
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+<head>
+    <!-- META SECTION -->
+    <title>Sciec - Acompanhe seus Eventos!</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-@section('content')
-    <div>
-        <br><br><br><br>
-    </div>
-    <div>
-        <br><br><br><br>
-    </div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <!-- END META SECTION -->
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail </label>
+    <!-- CSS INCLUDE -->
+    <link rel="stylesheet" type="text/css" id="theme" href="{{ asset('css/theme-default.css') }}"/>
+    <!-- EOF CSS INCLUDE -->
+</head>
+<body>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+<div class="login-container">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Senha</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+    <div class="login-box animated fadeInDown">
+        <div class="login-logo"></div>
+        <div class="login-body">
+            <div class="login-title"><strong>Bem Vindo!</strong> - Por favor, faça seu Login!</div>
+            <form action="{{ route('login') }}" class="form-horizontal" method="post">
+                {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="col-md-12">
+                        <input id=email type="email" class="form-control" name="email"
+                               placeholder="Insira seu e-mail..." value="{{ old('email') }}" required autofocus/>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <div class="col-md-12">
+                        <input id="password" type="password" class="form-control" name="password" placeholder="Senha"
+                               required/>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembrar Senha
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Esqueceu Sua Senha?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @endif
+                    </div>
                 </div>
+                <div class="form-group">
+                    <div class="col-md-6">
+                        <a href="{{ route('password.request') }}" class="btn btn-link btn-block">Esqueceu sua senha?</a>
+                    </div>
+                    <div class="col-md-6">
+                        <button class="btn btn-primary btn-block">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="login-footer">
+            <div class="pull-left">
+                &copy; 2017 Sciec
+            </div>
+            <div class="pull-right">
+                <a href="#">About</a> |
+                <a href="#">Privacidade</a> |
+                <a href="#">Contato</a>
+            </div>
+            <br><br>
+            <div class="col-md-12">
+                <a href="{{ url('register') }}"> <!--Adicionar Rotas-->
+                    <button class="btn btn-success btn-block">
+                        Cadastre-se
+                    </button>
+                </a>
             </div>
         </div>
     </div>
 </div>
-@endsection
+<!-- START PRELOADS -->
+<audio id="audio-alert" src="{{ asset('audio/alert.mp3') }}" preload="auto"></audio>
+<audio id="audio-fail" src="{{ asset('audio/fail.mp3') }}" preload="auto"></audio>
+<!-- END PRELOADS -->
+
+<!-- START SCRIPTS -->
+<!-- START PLUGINS -->
+<script type="text/javascript" src="{{ asset('js/plugins/jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/jquery/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/jquery/bootstrap.min.js') }}"></script>
+<!-- END PLUGINS -->
+
+<!-- START THIS PAGE PLUGINS-->
+<script type='text/javascript' src='{{ asset('js/plugins/icheck/icheck.min.js') }}'></script>
+<script type="text/javascript" src="{{ asset('js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/scrolltotop/scrolltopcontrol.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/plugins/morris/raphael-min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/morris/morris.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/rickshaw/d3.v3.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/rickshaw/rickshaw.min.js') }}"></script>
+<script type='text/javascript' src='{{ asset('js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}'></script>
+<script type='text/javascript' src='{{ asset('js/plugins/bootstrap/bootstrap-datepicker.js') }}'></script>
+<script type="text/javascript" src="{{ asset('js/plugins/owl/owl.carousel.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/plugins/moment.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins/daterangepicker/daterangepicker.js') }}"></script>
+<!-- END THIS PAGE PLUGINS-->
+
+<!-- START TEMPLATE -->
+<script type="text/javascript" src="{{ asset('js/settings.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/plugins.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/actions.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('js/demo_dashboard.js') }}"></script>
+<!-- END TEMPLATE -->
+<!-- END SCRIPTS -->
+</body>
+</html>
