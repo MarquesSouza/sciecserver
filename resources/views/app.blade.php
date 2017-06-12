1507cb7b279056c33a59a120aa920e9111db0d0f
krkbjj <krkbjj@gmail.com>
2017-06-12 01:45:17 -0300

    Mudança e Otimização do Template Correção forms evento/cad Correção de Botões e Rotas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -260,11 +260,6 @@
 </div>
 <!-- END MESSAGE BOX-->
 
-<!-- START PRELOADS -->
-<audio id="audio-alert" src="{{ asset('audio/alert.mp3') }}" preload="auto"></audio>
-<audio id="audio-fail" src="{{ asset('audio/fail.mp3') }}" preload="auto"></audio>
-<!-- END PRELOADS -->
-
 <!-- START SCRIPTS -->
 <!-- START PLUGINS -->
 <script type="text/javascript" src="{{ asset('js/plugins/jquery/jquery.min.js') }}"></script>
@@ -281,10 +276,7 @@
 <script type="text/javascript" src="{{ asset('js/plugins/morris/morris.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/plugins/rickshaw/d3.v3.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/plugins/rickshaw/rickshaw.min.js') }}"></script>
-<script type='text/javascript' src='{{ asset('js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}'></script>
-<script type='text/javascript' src='{{ asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}'></script>
 <script type='text/javascript' src='{{ asset('js/plugins/bootstrap/bootstrap-datepicker.js') }}'></script>
-<script type="text/javascript" src="{{ asset('js/plugins/owl/owl.carousel.min.js') }}"></script>
 
 <script type="text/javascript" src="{{ asset('js/plugins/moment.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/plugins/daterangepicker/daterangepicker.js') }}"></script>
