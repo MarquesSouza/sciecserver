4694c9a5c53d8840e112fd24aa3583a6ecda969d
marcos <marcossistemasifo@gmail.com>
2017-06-13 22:11:24 -0300

    teste

--- resources/views/evento/list_evento.blade.php
+++ resources/views/evento/list_evento.blade.php
@@ -1,43 +1,45 @@
 @extends('app')
 @section('content')
-    <div class="container">
+    <div class="page-content-wrap">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <br><br>
-                <a href="{{ url('evento/cad') }}" class="btn btn-default">Novo Evento</a>
-                <br><br>
-                <table class="table table-bordered table-inverse">
-                    <th>Nome </th>
-                    <th>Descricão </th>
-                    <th>Local </th>
-                    <th>Data de Início </th>
-                    <th>Data de Conclusão</th>
-                    <th>Logo do Evento</th>
+            <div class="col-md-12">
+                <!-- START DEFAULT DATATABLE -->
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="{{ url('evento/cad') }}" class="btn btn-primary ">Novo Evento</a>
+                        <table class="table table-bordered ">
+                            <th>Nome</th>
+                            <th>Descricão</th>
+                            <th>Local</th>
+                            <th>Data de Início</th>
+                            <th>Data de Conclusão</th>
+                            <th>Logo do Evento</th>
+                            <th>Edita</th>
+                            <th>Excluir</th>
+                            <tr>
+                                @forelse ($events as $ev)
 
-                     <tr>
-                        @forelse ($events as $ev)
+                                    <td>{{ $ev->nome }}</td>
+                                    <td>{{ $ev->descricao }}</td>
+                                    <td>{{ $ev->local }}</td>
+                                    <td>{{ $ev->data_inicio }}</td>
+                                    <td>{{ $ev->data_conclusao }}</td>
+                                    <td>{{ $ev->logoEvento }}</td>
+                                    <td>
+                                        <a href="{{url('usuario/config/edit/{id}')}}" class="btn btn-success">EDITAR</a>
+                                    </td>
+                                    <td>
+                                        <a href="{{url('usuario/config/edit/{id}')}}" class="btn btn-danger">EXCLUIR</a>
+                                    </td>
 
-                                 <td>{{ $ev->nome }}</td>
-                            <td>{{ $ev->descricao }}</td>
-                            <td>{{ $ev->local }}</td>
-                            <td>{{ $ev->data_inicio }}</td>
-                            <td>{{ $ev->data_conclusao }}</td>
-                            <td>{{ $ev->logoEvento }}</td>
-                                 <td>
-                                     <a href="{{url('usuario/config/edit/{id}')}}" class="btn-success btn btn-default btn-sm">EDITAR</a>
-                                 </td>
-                                 <td>
-                                     <a href="{{url('usuario/config/edit/{id}')}}" class="btn danger-color  btn-default btn-sm">EXCLUIR</a>
-                                 </td>
+                            </tr>
+                            @empty
+                                <p>No events</p>
+                            @endforelse
 
-                    </tr>
-                    @empty
-                        <p>No events</p>
-                    @endforelse
+                        </table>
 
-                    </table>
-
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 @endsection
