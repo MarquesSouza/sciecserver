3afa79cbdfb61170831306ec0c6dc83e819566b6
marcos <marcossistemasifo@gmail.com>
2017-06-18 17:18:25 -0300

    tabelas

--- resources/views/tipo_atividade_usuario/_form.blade.php
+++ resources/views/tipo_atividade_usuario/_form.blade.php
@@ -4,5 +4,9 @@
         <label class="col-md-4 control-label" for="singlebutton"></label>
         <div class="col-md-4">
             <button id="singlebutton" name="singlebutton" class="btn btn-success">Cadastrar</button>
+            <a href="{{('tipo_atividade_usuario/list_tipo_atividade_usuario')}}">
+                <button id="singlebutton" name="singlebutton" class="btn btn-danger">Voltar</button>
+            </a>
+
         </div>
     </div>
