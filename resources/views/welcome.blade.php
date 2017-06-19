7347d25bb5d42d85e62b3411d002a24dfca77c6c
Marques <marquesde2013@gmail.com>
2017-06-19 16:18:28 -0300

    Atividade Controler

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
     <div class="content text-center">
         <div class="row">
             @forelse ($evento as $i)
-                <div class="col-sm-6 col-md-4">
+                @if($i->status==1)
+
+                    <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
                         <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%"
                              alt="Image">
@@ -14,13 +16,13 @@
                         <a href="{{url('evento/detalhar_evento/'.$i->id)}}" class="btn btn-default" role="button">Mais
                             Informações</a></p>
                 </div>
+
+             @endif
+
             @empty
                 <p>Nenhum evento cadastrado</p>
             @endforelse
-            </div>
-        </div>
-    </div>
-    <!--Lista de Eventos-->
 
+    </div>
     </div>
 @endsection
